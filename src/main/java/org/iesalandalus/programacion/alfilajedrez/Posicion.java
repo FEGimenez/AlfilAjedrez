7608ad75f4d1d05960e569024ce9cdc1772045ad
Fer <Fer@DESKTOP-93RMAV5>
2020-11-25 18:46:16 +0100

    Corrijo toString

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -58,7 +58,7 @@ public class Posicion {
 
 		@Override
 		public String toString() {
-			return String.format("[fila=%s, columna=%s]", fila, columna);
+			return String.format("fila=%s, columna=%s", fila, columna);
 		}
 		
 		
