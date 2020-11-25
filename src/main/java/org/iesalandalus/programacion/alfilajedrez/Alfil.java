070a91d7d5d209a59064454cca46bee32c951a4c
Fer <Fer@DESKTOP-93RMAV5>
2020-11-25 20:34:10 +0100

    Creo los métodos get y set y mensaje null

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -4,6 +4,30 @@ public class Alfil {
 	
 	private Color color;
 	private Posicion posicion;
-	
 
+	
+	public Alfil(Color color, Posicion posicion) {
+		this.color = color;
+		this.posicion = posicion;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			this.color = color;
+		}
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	
+	}
 }
